28b416552b32e91b0e6f9edb31a120aa25e20238
fonsekaua <fonsecakauasilva3@gmail.com>
2025-11-26 03:51:47 -0500

    (Feat) criando as rotas e controllers

--- src/controllers/User/index.ts
+++ src/controllers/User/index.ts
@@ -3,3 +3,19 @@ import { RequestHandler } from "express";
 export const getAllUsers: RequestHandler = async (req, res) => {
     res.send("Get all users");
 }
+
+export const getUserByEmail: RequestHandler = async (req, res) => {
+    res.send(`Get user by email`);
+}
+
+export const createUser: RequestHandler = async (req, res) => {
+    res.send("Create user");
+}
+
+export const editUser: RequestHandler = async (req, res) => {
+    res.send("Edit user");
+}
+
+export const deleteUser: RequestHandler = async (req, res) => {
+    res.send("Delete user");
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,5 +2,8 @@ import { Router } from 'express'
 import * as User from '../controllers/User/index.js';
 
 export const App = Router();
-
 App.get("/users", User.getAllUsers);
+App.get("/user/:email", User.getUserByEmail);
+App.post("/user", User.createUser);
+App.put("/user", User.editUser);
+App.delete("/user", User.deleteUser);
